d48e86e67128e8be6bc4aedc9ced8a16355698d4
Mohammed harshad pk <mycks45@gmail.com>
2021-06-20 00:24:00 +0530

    first commit

---
+++ chatapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<str:room>/', views.room, name= 'room'),
+    path('check_value', views.checkValue, name='check_value'),
+    path('send', views.send, name='send')
+]
